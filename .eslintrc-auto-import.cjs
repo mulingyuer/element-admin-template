9763d39b1e7380314f24788973c1aa5510b1d06d
mulingyuer <1321968423@qq.com>
2025-01-17 10:55:26 +0800

    feat: 补充类型文件

--- .eslintrc-auto-import.cjs
+++ .eslintrc-auto-import.cjs
@@ -5,8 +5,6 @@ module.exports = {
     "ComputedRef": true,
     "DirectiveBinding": true,
     "EffectScope": true,
-    "ElMessage": true,
-    "ElNotification": true,
     "ExtractDefaultPropTypes": true,
     "ExtractPropTypes": true,
     "ExtractPublicPropTypes": true,
