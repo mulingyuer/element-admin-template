9e1cece4276d41c18e9b946ea4a868a459529afa
木灵鱼儿 <50077795+mulingyuer@users.noreply.github.com>
2025-12-25 10:20:15 +0800

    feat(utils): 扩展formatDate函数支持多种日期类型
    - 修改formatDate函数参数类型，支持Date、string和number类型
    - 更新文件最后编辑时间戳

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: mulingyuer
  * @Date: 2024-10-11 17:10:47
- * @LastEditTime: 2024-10-11 17:10:47
+ * @LastEditTime: 2025-12-25 10:20:05
  * @LastEditors: mulingyuer
  * @Description: dayjs封装
  * @FilePath: \element-admin-template\src\utils\dayjs.ts
@@ -12,6 +12,6 @@ import "dayjs/locale/zh-cn";
 dayjs.locale("zh-cn");
 
 /** 格式化日期 */
-export function formatDate(date: Date, format: string): string {
+export function formatDate(date: Date | string | number, format: string): string {
 	return dayjs(date).format(format);
 }
